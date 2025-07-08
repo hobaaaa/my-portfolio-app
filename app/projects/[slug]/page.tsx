a707d0b45e43ae97ddb906f46b1cae014bb501b4
hobaaaa <denizgokbudak@gmail.com>
2025-07-08 15:12:02 +0300

    fix- bug fix slug

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,8 +2,14 @@ import Navbar from "@/app/components/Navbar";
 import { workData } from "@/assets/assets";
 import Image from "next/image";
 import { notFound } from "next/navigation";
-
-export default function ProjectPage({ params }: { params: { slug: string } }) {
+import { use } from "react";
+type ProjectParams = {
+  slug: string;
+};
+export default function ProjectPage(
+  promiseParams: Promise<{ params: ProjectParams }>
+) {
+  const { params } = use(promiseParams);
   const project = workData.find((p) => p.slug === params.slug);
   if (!project) return notFound();
 
